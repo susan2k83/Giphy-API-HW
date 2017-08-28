8ff5d734298498bc8d12d7b5bae5ca9afde25a48
susan2k83 <susan2k83@yahoo.com>
2017-08-27 23:15:25 -0700

    more jquery added, buttons up, but random gifs appear

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,71 +1,89 @@
 $(document).ready(function() {
 
-// Api Key: eb8e89c091bd409eb627bb0b7594c298
+            // Api Key: eb8e89c091bd409eb627bb0b7594c298
 
-// page loads with a row of buttoms already at the top
+            // page loads with a row of buttoms already at the top
 
-// these buttons are made in jquery
+            // these buttons are made in jquery
 
-// on click one the buttons
+            // on click one the buttons
 
-// the screen populates with 10 still gifs
+            // the screen populates with 10 still gifs
 
-// the rating is located below the image
+            // the rating is located below the image
 
-// on click the gif, the gif animates
+            // on click the gif, the gif animates
 
-// click again, the gif is still
+            // click again, the gif is still
 
-// click a different gif, the current screen of gifs goes away
-// and the new gifs appear...
+            // click a different gif, the current screen of gifs goes away
+            // and the new gifs appear...
 
-var humor = ["Spongebob", "Karate Fails", "Surprise"];
+            var humor = ["spongebob", "surprise", "karate fails"];
 
-renderButtons();
+            showButtons();
 
-function renderButtons() {
+            function showButtons() {
 
-	$(".gifButton").empty();
+                // $(".gifButtons").empty();
 
-	for(var i = 0; i < humor.length; i++) {
+                for (var i = 0; i < humor.length; i++) {
 
-		 var a = $("<button>");
-		 a.addClass("gif");
-		 a.attr("data-name", humor[i]);
-		 a.text(humor[i]);
-		 $("gifButton").append(a);
+                    var a = $("<button>");
+                    a.addClass("humor");
+                    // the attribute is not being applied  to the buttons as seen in the console
 
-	}
+                    a.attr("data-name");
+                    a.text(humor[i]);
+                    $(".gifButtons").append(a);
 
-}
+                }
 
-
-$("button").on("click", function(){
-
-	var humor = $(this).data("search");
-});
- 
+            }
 
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?q="+humor+"api_key=eb8e89c091bd409eb627bb0b7594c298&limit=10";
-console.log(queryURL);
+            $("button").on("click", function(event) { 
+            	event.preventDefault();
 
-$.ajax({
-	url:queryURL,
-	method:"GET"
-}).done(function(response) {
-	console.log(response);
-});
+                var humor = $(this).attr("data-name");
+            // });
+               
 
 
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q="+humor+"&api_key=eb8e89c091bd409eb627bb0b7594c298&limit=10";
+            console.log(queryURL);
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).done(function(response) {
+                console.log(response);
+            // });
+            	var results = response.data;
+
+            for (i = 0; i<results.length; i++){
+                $(".gifImages").append("<img src = ' " + response.data[i].images.fixed_height.url + " '>");
+                $("img").addClass("gif");
+
+                $(".gifImages").append("<p>Rating: " + response.data[i].rating);
+            };
+           });
+	     });
+             
+            $(".gif").on("click", function() {
+
+            	var state = $(this).attr("data-state");
+            	if (state === "still") {
+            		$(this).attr("src", $(this).attr("data-animate"));
+            		$(this).attr("data-state", "animate");            	
+            	} else {
+            		$(this).attr("src", $(this).attr("data-still"));
+            		$(this).attr("data-state", "still");
+            	}
 
 
+          
 
+            });
 
-
-
-
-
-
-
-	});
+        });
