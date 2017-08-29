eaf2eadfe21c76d60e788d6c03409680c78357a6
susan2k83 <susan2k83@yahoo.com>
2017-08-28 22:00:42 -0700

    final commit for homework

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,89 +1,88 @@
 $(document).ready(function() {
 
-            // Api Key: eb8e89c091bd409eb627bb0b7594c298
-
-            // page loads with a row of buttoms already at the top
-
-            // these buttons are made in jquery
-
-            // on click one the buttons
-
-            // the screen populates with 10 still gifs
-
-            // the rating is located below the image
-
-            // on click the gif, the gif animates
-
-            // click again, the gif is still
-
-            // click a different gif, the current screen of gifs goes away
-            // and the new gifs appear...
-
-            var humor = ["spongebob", "surprise", "karate fails"];
-
-            showButtons();
-
-            function showButtons() {
-
-                // $(".gifButtons").empty();
-
-                for (var i = 0; i < humor.length; i++) {
-
-                    var a = $("<button>");
-                    a.addClass("humor");
-                    // the attribute is not being applied  to the buttons as seen in the console
-
-                    a.attr("data-name");
-                    a.text(humor[i]);
-                    $(".gifButtons").append(a);
-
-                }
-
-            }
+    // Api Key: eb8e89c091bd409eb627bb0b7594c298
 
 
-            $("button").on("click", function(event) { 
-            	event.preventDefault();
 
-                var humor = $(this).attr("data-name");
-            // });
-               
+    var humor = ["spongebob", "surprise", "karate fails"];
+
+    showButtons();
+
+    function showButtons() {
+
+        // $(".gifButtons").empty();
+
+        for (var i = 0; i < humor.length; i++) {
+
+            var a = $("<button>");
+            a.addClass("newGif");
+            a.attr("data-name", humor[i]);
+            a.text(humor[i]);
+            $(".gifButtons").append(a);
+
+        }
+
+    }
 
 
-            var queryURL = "https://api.giphy.com/v1/gifs/search?q="+humor+"&api_key=eb8e89c091bd409eb627bb0b7594c298&limit=10";
-            console.log(queryURL);
+    $("button").on("click", function(event) {
+        event.preventDefault();
 
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).done(function(response) {
-                console.log(response);
-            // });
-            	var results = response.data;
+        var humor = $(this).attr("data-name");
+        // });
 
-            for (i = 0; i<results.length; i++){
-                $(".gifImages").append("<img src = ' " + response.data[i].images.fixed_height.url + " '>");
+
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + humor + "&api_key=eb8e89c091bd409eb627bb0b7594c298&limit=10";
+        console.log(queryURL);
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function(response) {
+            console.log(response);
+            // })   ;
+            var results = response.data;
+            $(".gifImages").empty();
+            for (i = 0; i < results.length; i++) {
+                $(".gifImages").prepend("<img src = ' " + response.data[i].images.fixed_height.url + " '>");
                 $("img").addClass("gif");
 
-                $(".gifImages").append("<p>Rating: " + response.data[i].rating);
+                $(".gifImages").prepend("<p>Rating: " + response.data[i].rating);
             };
-           });
-	     });
-             
-            $(".gif").on("click", function() {
-
-            	var state = $(this).attr("data-state");
-            	if (state === "still") {
-            		$(this).attr("src", $(this).attr("data-animate"));
-            		$(this).attr("data-state", "animate");            	
-            	} else {
-            		$(this).attr("src", $(this).attr("data-still"));
-            		$(this).attr("data-state", "still");
-            	}
-
-
-          
-
-            });
-
         });
+    });
+
+    // search for new gif form input
+    $("#add-gif").on("click", function(event) {
+
+        event.preventDefault();
+        var newGif = $("#search-input").val().trim();
+        humor.push(newGif);
+        // this part not working quite right.When a user types in a search in the search field,it does create a new button,but ends up duplicating all of the other buttons too..
+        // Tried to use $("#search-input").empty();
+        showButtons();
+
+
+    });
+
+
+    // Not able to get this code to work either..
+    // 
+    $(".gif").on("click", function() {
+
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+
+
+
+
+    });
+
+});
